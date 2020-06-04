1cdab44d1516d0c9c23e7cc601ed9be5414e5d1f
Kaitlin Hill <hillkai@flip2.engr.oregonstate.edu>
2020-06-04 14:19:30 -0700

    updated basics folder

---
+++ C++Basics/HelloWorld.cpp
@@ -0,0 +1,14 @@
+/******************************************
+ * Program: C++ Basics (1) - Hello World
+ * Author: TALI OSU
+ * Description: How to write a basic hello world C++ program using cout.
+ * Video Link:
+ * **************************************/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    return 0;
+}
