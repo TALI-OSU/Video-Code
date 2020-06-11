1d589a8167fa81f1b75ddbb384eeda3b73e6b65f
Aiden Nelson <nelsonai@oregonstate.edu>
2020-06-11 11:15:24 -0700

    Add files via upload

---
+++ C++Basics/variable_demo.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int myIntVar;
+    myIntVar = 3;
+
+    float myFloatVar = 2.2;
+
+    bool myBoolVar = true;
+
+    char myCharVar = '8';
+
+    string myStringVar = "Hello there!";
+
+    cout << myIntVar << endl;
+    cout << myFloatVar << endl;
+    cout << myBoolVar << endl;
+    cout << myCharVar << endl;
+    cout << myStringVar << endl;
+
+    return 0;
+}
